d8e57415ebe3411f4ca0c826d3a0270cee7024b4
seifsay3d <seifsayed@gmail.com>
2019-04-15 01:31:19 +0200

    Use Header

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,23 @@
 <template>
   <div id="app">
+    <app-header></app-header>
     <router-view></router-view>
   </div>
 </template>
 
-<style lang="scss"></style>
+<script lang="ts">
+import Vue from "vue";
+import AppHeader from "@/components/layout/Header.vue";
+import Component from "vue-class-component";
+
+@Component({
+  components: {
+    AppHeader
+  }
+})
+export default class App extends Vue {}
+</script>
+
+<style lang="scss">
+@import "~normalize.css";
+</style>
