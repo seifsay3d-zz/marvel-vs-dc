5d5a9738b56f77c15202c9c65af6e3654a5cd7ba
seifsay3d <seifsayed@gmail.com>
2019-04-25 00:01:41 +0200

    Temporarly add button styles to app
    I don't really like how styled components are integrated
    will take a decision later

--- src/App.vue
+++ src/App.vue
@@ -20,4 +20,33 @@ export default class App extends Vue {}
 
 <style lang="scss">
 @import "~normalize.css";
+@import url("https://fonts.googleapis.com/css?family=Noto+Sans");
+
+body {
+  font-family: "Noto Sans", sans-serif;
+}
+
+.button {
+  display: inline-block;
+  background: #ffc500;
+  padding: 10px 25px;
+  border-radius: 5px;
+  border: 0px;
+  text-decoration: none;
+  color: black;
+  text-align: center;
+  font-size: 14px;
+  text-transform: uppercase;
+
+  &--has-checkbox {
+    input {
+      display: none;
+    }
+  }
+
+  &--is-full-width {
+    padding: 10px 0px;
+    width: 100%;
+  }
+}
 </style>
