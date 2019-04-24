17b1e9bfb9d9cc1acbc45e6764e022ddb4fb1952
seifsay3d <seifsayed@gmail.com>
2019-04-24 23:57:52 +0200

    Add slider and slide components

---
+++ src/components/SSlide.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="slide">
+    <slot></slot>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+@Component({})
+export default class Slide extends Vue {}
+</script>
+<style lang="scss">
+.slide {
+  width: 100%;
+  flex-shrink: 0;
+}
+</style>
---
+++ src/components/SSlider.vue
@@ -0,0 +1,148 @@
+<template>
+  <div class="slider">
+    <div class="slider__slides">
+      <slot></slot>
+    </div>
+    <button
+      class="slider__button slider__button--prev"
+      :disabled="activeSlideIndex - 1 < 0"
+      @click="updateSlideIndex(-1)"
+    ><</button>
+    <button
+      :disabled="activeSlideIndex + 1 >= slides.length"
+      class="slider__button slider__button--next"
+      @click="updateSlideIndex(1)"
+    >></button>
+    <ul class="slider__dots">
+      <li
+        v-for="(slide, index) in slides"
+        :key="index"
+        @click="setSlideIndex(index)"
+        :class="{ 'slider__dots--active': index == activeSlideIndex }"
+      ></li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+
+@Component({
+  provide() {
+    return {
+      slider: this
+    };
+  },
+  watch: {
+    activeSlideIndex: {
+      handler: function() {
+        this.slides.forEach((item, index) => {
+          if (this.activeSlideIndex == index) {
+            item.$el.style.display = "block";
+          } else {
+            item.$el.style.display = "none";
+          }
+        });
+      },
+      immediate: false
+    }
+  }
+})
+export default class Slider extends Vue {
+  slides = [];
+  activeSlideIndex = 0;
+  mounted() {
+    this.slides =
+      this.$slots && this.$slots.default
+        ? this.$slots.default.map(s => s.componentInstance)
+        : [];
+  }
+  updateSlideIndex(direction: number) {
+    this.activeSlideIndex += direction;
+  }
+  setSlideIndex(index: number) {
+    this.activeSlideIndex = index;
+  }
+}
+</script>
+
+<style lang="scss">
+$slider-dot-size: 15px;
+$control-color: #fff;
+
+.slider {
+  display: flex;
+  position: relative;
+  background: #111;
+  height: 550px;
+  margin-top: 10px;
+  padding: 0 100px;
+
+  &__slides {
+    width: 100%;
+
+    display: flex;
+    overflow: hidden;
+  }
+
+  &__button {
+    position: absolute;
+    font-size: 25px;
+    color: $control-color;
+    width: 50px;
+    height: 50px;
+    background: transparent;
+    border-radius: 50%;
+    border: 1px solid $control-color;
+
+    &--prev {
+      left: 20px;
+      top: calc(50% - 25px);
+    }
+
+    &--next {
+      right: 20px;
+      top: calc(50% - 25px);
+    }
+  }
+
+  &__dots {
+    list-style: none;
+    text-align: center;
+    position: absolute;
+    bottom: 10px;
+    width: 100px;
+    left: calc(50% - 50px);
+    margin: 0px;
+    padding: 0px;
+    li {
+      position: relative;
+      display: inline-block;
+      margin: 0 5px;
+      border: 1px solid $control-color;
+      border-radius: 50%;
+      width: $slider-dot-size;
+      height: $slider-dot-size;
+
+      cursor: pointer;
+      &:before {
+        position: absolute;
+        content: "";
+        border-radius: 50%;
+        width: 100%;
+        height: 100%;
+        left: 0;
+
+        background: transparent;
+      }
+
+      &.slider__dots--active {
+        &:before {
+          background: $control-color !important;
+        }
+      }
+    }
+  }
+}
+</style>
