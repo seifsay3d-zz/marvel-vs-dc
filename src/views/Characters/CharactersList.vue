1658a521570397fb0b1a93daaa3dd74b00101646
seifsay3d <seifsayed@gmail.com>
2019-04-26 00:52:06 +0200

    Uplift CharactersList - add header - remove unneccessary css

--- src/views/Characters/CharactersList.vue
+++ src/views/Characters/CharactersList.vue
@@ -1,7 +1,9 @@
 <template>
-  <div>
-    <div class="characters_list">
+  <dc-container>
+    <dc-header>Characters</dc-header>
+    <div class="characters-list">
       <character-thumb
+        class="characters-list__character"
         v-for="char in characters"
         :key="char.id"
         :character="char"
@@ -12,10 +14,12 @@
     </div>
     <transition name="fade">
       <div class="match_button" v-if="isMatchReady">
-        <button class="button" @click="goToMatch">See whos' better!</button>
+        <button class="button button--danger" @click="goToMatch">
+          See whos' better!
+        </button>
       </div>
     </transition>
-  </div>
+  </dc-container>
 </template>
 <script lang="ts">
 import Vue from "vue";
@@ -95,50 +99,20 @@ export default class CharacterList extends Vue {
 </script>
 
 <style lang="scss" scoped>
-.characters_list {
+.characters-list {
   display: flex;
   flex-wrap: wrap;
   justify-content: space-between;
-}
-.content {
-  height: 100%;
-  display: flex;
-
-  &__left {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    color: white;
-    flex-basis: 50%;
-    flex-grow: 1;
-  }
-  &__right {
-    display: flex;
-    flex-basis: 50%;
-    flex-grow: 1;
-    width: 50%;
-  }
-
-  &__img {
-    display: block;
-    width: 100%;
-    height: auto;
+  &__character {
+    box-sizing: border-box;
+    width: 30%;
+    max-width: 30%;
   }
 }
-
 .match_button {
   position: fixed;
   right: 0;
   top: 80px;
   padding: 10px;
-}
-
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 0.5s;
-}
-.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
-  opacity: 0;
 }
 </style>
