c36660c607c81805fe74e8895c1f3bfdc0618a9b
seifsay3d <seifsayed@gmail.com>
2019-04-25 15:46:05 +0200

    Add Character Search

---
+++ src/components/CharacterSearch.vue
@@ -0,0 +1,29 @@
+<template>
+  <form @submit.prevent="onSubmit">
+    <input v-model="characterName" placeholder="search" />
+  </form>
+</template>
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+import MDCInput from "@/components/user-interface/mdc-input";
+
+@Component({
+  components: {
+    "mdc-input": MDCInput
+  }
+})
+export default class ChracterSearch extends Vue {
+  characterName = null;
+
+  onSubmit() {
+    this.$router.push({
+      name: "characters",
+      query: {
+        name: this.characterName
+      }
+    });
+    this.characterName = null;
+  }
+}
+</script>
