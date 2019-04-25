53dbc08347f12928b31417191b4fbfaa048f1a15
seifsay3d <seifsayed@gmail.com>
2019-04-25 15:48:38 +0200

    Create Home page component

---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <home-slider></home-slider>
+    <strongest-characters></strongest-characters>
+  </div>
+</template>
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+import StrongestCharacters from "@/components/StrongestCharacters.vue";
+import HomeSlider from "@/components/HomeSlider.vue";
+@Component({
+  components: {
+    HomeSlider,
+    StrongestCharacters
+  }
+})
+export default class Home extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+</style>
